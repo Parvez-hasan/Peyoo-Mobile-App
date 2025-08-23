fbd2981c7e3a6950410016881cb7addede689e04
Parvez-hasan <parvez7788@gmail.com>
2025-08-23 19:09:24 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+/// login button functionality
+
+document.getElementById("loginButton").addEventListener("click", function(e){
+    e.preventDefault()
+
+
+    // mobile number 
+const mobileNumber = 1948017882;
+const pinNumber = 2244;
+
+const mobileNumberValue = document.getElementById("mobile-number").value
+
+//console.log(mobileNumberValue).value
+const mobileNumberValueConvert = parseInt(mobileNumberValue)
+
+// pin number
+
+const pinNumberValue = document.getElementById("pin-number").value
+const pinNumberValueConvert = parseInt (pinNumberValue)
+
+console.log(mobileNumberValueConvert, pinNumberValueConvert);
+
+if(mobileNumberValueConvert === mobileNumber && pinNumberValueConvert === pinNumber){
+
+    //console.log("all value mess");
+    window.location.href = "./home.html"
+    
+}
+else{
+    
+    alert("Please Inter Right Number")
+    
+}
+
+
+
+
+})
